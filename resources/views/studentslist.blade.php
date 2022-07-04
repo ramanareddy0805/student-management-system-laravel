0be9c169c523aa5f247d0ea8fdeb995751626525
Ramana Reddy <ramanareddylpu@gmail.com>
2022-07-04 11:28:39 +0530

    master commit

---
+++ resources/views/studentslist.blade.php
@@ -0,0 +1,39 @@
+<div class="card mb-3">
+    <img src="https://visme.co/blog/wp-content/uploads/2020/06/animated-interactive-infographics-header.gif" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">List of students</h5>
+        <p class="card-text">This is the information about all the students</p>
+
+        <table class="table">
+            <thead class="thead-light">
+            <tr>
+                <th scope="col">CNE</th>
+                <th scope="col">First name</th>
+                <th scope="col">Second Name</th>
+                <th scope="col">Age</th>
+                <th scope="col">Speciality</th>
+                <th scope="col">Operations</th>
+
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($students as $student)
+                <tr>
+                    <td>{{ $student->cne }}</td>
+                    <td>{{ $student->firstName }}</td>
+                    <td>{{ $student->secondName }}</td>
+                    <td>{{ $student->age }}</td>
+                    <td>{{ $student->speciality }}</td>
+                    <td>
+
+                        <a href="{{ url('/edit/'.$student->id) }}" class="btn btn-sm btn-warning">Edit</a>
+
+                    </td>
+
+
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
